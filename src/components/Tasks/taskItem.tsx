83ec503b41f3eba85b42239d9d7d31ba62b6c2b5
kaushalrijal <kaushalrijal091@gmail.com>
2024-07-15 18:57:26 +0545

    add priority

--- src/components/Tasks/taskItem.tsx
+++ src/components/Tasks/taskItem.tsx
@@ -1,47 +1,83 @@
-import React, { useState } from 'react'
-import { Checkbox } from '../ui/checkbox'
-import { Label } from '../ui/label'
-import { Pencil1Icon, TrashIcon } from '@radix-ui/react-icons'
+import React, { useState } from "react";
+import { Checkbox } from "../ui/checkbox";
+import { Label } from "../ui/label";
+import { Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
+import { Badge } from "../ui/badge";
 
-const TaskItem = (props) => {
-  const {title, description, priority, isComplete, _id} = props.taskData;
-  const[checked, setChecked] = useState(isComplete)
+type PriorityType = "outline" | "secondary" | "default" | "destructive";
+
+interface Priority {
+  type: PriorityType;
+  value: string;
+}
+
+const priorities: Record<number, Priority> = {
+  1: {
+    type: "outline",
+    value: "Low",
+  },
+  2: {
+    type: "secondary",
+    value: "Medium",
+  }, 
+  3: {
+    type: "default",
+    value: "Normal",
+  },
+  4: {
+    type: "destructive",
+    value: "High",
+  },
+  5: {
+    type: "destructive",
+    value: "Very High",
+  },
+};
+
+const TaskItem = (props: {
+  taskData: {
+    title: any;
+    description: any;
+    priority: any;
+    isComplete: any;
+    _id: any;
+    onDelete: any;
+  };
+  onDelete: (arg0: any) => void;
+}) => {
+  const { title, description, priority, isComplete, _id, onDelete } =
+    props.taskData;
+  const [checked, setChecked] = useState(isComplete);
   return (
-    <div className='w-full border border-black p-2 my-2 rounded-md flex'>
-      <Label htmlFor={_id} className='flex w-1/6 items-center justify-center'>
-      <Checkbox
-        id={_id}
-        className='h-6 w-6'
-        checked={checked}
-        onCheckedChange={() => setChecked(!checked)}
-      />
+    <div className="w-full border border-black p-2 my-2 rounded-md flex">
+      <Label htmlFor={_id} className="flex w-1/6 items-center justify-center">
+        <Checkbox
+          id={_id}
+          className="h-6 w-6"
+          checked={checked}
+          onCheckedChange={() => setChecked(!checked)}
+        />
       </Label>
-      <Label htmlFor={_id} className='w-3/6'>
-      <h2 className='font-semibold'>{title}</h2>
-      <h4 className='text-muted-foreground'>{description}</h4>
+      <Label htmlFor={_id} className="w-3/6">
+        <h2 className="font-semibold">{title}&nbsp;
+          <Badge variant={`${priorities[priority].type}`}>{priorities[priority].value}</Badge>
+        </h2>
+        <h4 className="text-muted-foreground">{description}</h4>
       </Label>
-      <div className='w-2/6 flex gap-2 justify-end items-center'>
-        <Pencil1Icon className='w-8 h-8 bg-black text-white rounded-full p-1.5 cursor-pointer' onClick={()=>{console.log("testing")}}/>
-        <TrashIcon className='w-8 h-8 bg-black text-white rounded-full p-1.5 cursor-pointer' onClick={() => deleteItem(_id)}/>
+      <div className="w-2/6 flex gap-2 justify-end items-center">
+        <Pencil1Icon
+          className="w-8 h-8 bg-black text-white rounded-full p-1.5 cursor-pointer"
+          onClick={() => {
+            console.log("testing");
+          }}
+        />
+        <TrashIcon
+          className="w-8 h-8 bg-black text-white rounded-full p-1.5 cursor-pointer"
+          onClick={() => props.onDelete(_id)}
+        />
       </div>
-      
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-const deleteItem = async (id: any) => {
-  const dlt = await fetch("http://localhost:5000/tasks/delete", {
-    
-      method : "POST",
-      headers : {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({id})
-    
-  })
-
-  const res = await dlt.json();
-  console.log(res)
-}
-
-export default TaskItem
+export default TaskItem;
--- src/components/Tasks/tasks.tsx
+++ src/components/Tasks/tasks.tsx
@@ -7,11 +7,12 @@ interface Task {
   title: string;
   priority: number;
   description: string;
+  isComplete: boolean;
   __v: number;
 }
 
 const Tasks = () => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   useEffect(() => {
     const getTasks = async () => {
@@ -25,19 +26,34 @@ const Tasks = () => {
     } catch (err) {
       console.log(err);
     }
-  }, []);
+  }, [tasks]);
+
+  const deleteItem = async (id: any) => {
+    const dlt = await fetch("http://localhost:5000/tasks/delete", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id }),
+    });
+
+    const res = await dlt.json();
+    console.log(res);
+
+    setTasks((tasks) => tasks.filter((todo) => todo._id !== id));
+  };
 
   return (
     <div className="p-4">
       <h2 className="font-semibold ">Tasks</h2>
       <div className="grid md:grid-cols-2 md:gap-x-4">
-      {tasks.map((task: Task) => {
-        return (
-          <Suspense key={task._id} fallback={<Loading />}>
-            <TaskItem taskData={task} idt={task._id} />
-          </Suspense>
-        );
-      })}
+        {tasks.map((task: Task) => {
+          return (
+            <Suspense key={task._id} fallback={<Loading />}>
+              <TaskItem taskData={task} onDelete={deleteItem}/>
+            </Suspense>
+          );
+        })}
       </div>
     </div>
   );
